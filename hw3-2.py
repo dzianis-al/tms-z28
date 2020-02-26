35b82e9f1e5b84b8bcce030d300c67e85a3d28ba
A.Den <mr.a.den3508@gmail.com>
2020-02-27 02:55:25 +0300

    the task2

---
+++ hw3-2.py
@@ -0,0 +1,9 @@
+guests = int(input('enter the number of guests:\n'))
+if guests >= 50:
+    print('need to order a restaurant:')
+elif guests > 20 and guests :
+     print('need to order a cafe:\n')
+else:
+      print('stay at home:\n')
+
+
